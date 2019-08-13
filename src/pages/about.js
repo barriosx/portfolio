615bf46d90c73a3a70ee3bd92f0b3f265e351ce9
barriosx <stevenbarrios03@gmail.com>
2019-08-13 18:29:11 -0400

    Begin work on About section.
    Create css module and put information onto jsx. Need to bring in profile image and it should be complete.

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,13 +2,30 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import about from "../styles/about.module.css"
+
 
 const About = () => (
   <Layout>
     <SEO title="About" />
     <div className="page-wrapper">
-      <h1>Hi from the about page</h1>
-      <p>Welcome to the about page</p>
+      <div className={about.center}>
+        <h1>Hey there!</h1>
+        <p>I'm glad you want to get more familiar. Here's a quick overview of who I am:</p>
+        <ul>
+          <li>
+            I was born and currently live in NY, and am of Guatemalan and Salvadoran ethnicity <span role="img" aria-label="Guatemala">🇬🇹</span> <span role="img" aria-label="El Salvador">🇸🇻</span>
+          </li>
+          <li>  
+            I like to travel <span role="img" aria-label="Plane Emoji">✈️</span> and I consider myself a foodie <span role="img" aria-label="Silverware Emoji">🍴</span>
+          </li>
+          <li>
+            I listen to EDM, Hip-Hop, and latin music <span role="img" aria-label="Headphones Emoji">🎧</span>
+          </li>
+        </ul>
+        <p>If that isn't the kind of information you're looking for, my resume might be a better option.</p>
+        <p>If that isn't enough information, add me on my social media accounts!</p>
+      </div>  
     </div>
   </Layout>
 )
