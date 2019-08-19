98eb589df05e267bf7bc0df98b34e33b31c57c48
barriosx <stevenbarrios03@gmail.com>
2019-08-19 18:09:13 -0400

    Change text sizes and navbar

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,8 +6,7 @@ import Card from "../components/card";
 import projects from "../styles/projects.module.css"
 import SmartchargeImage from "../components/smartcharge-img";
 import EzparknImage from "../components/ezparkn-img";
-// import ezparknGIF from "../images/ezparkn.gif"
-// import smartchargeGIF from "../images/smartcharge.gif"
+
 const Projects = () => (
   <Layout>
     <SEO title="Projects" />
@@ -17,7 +16,6 @@ const Projects = () => (
         <div className={projects.project}>
           <div className={projects.projectImage}>
             <SmartchargeImage />
-            {/* <img src={smartchargeGIF} className={projects.img} alt="Smartcharge Plus"></img> */}
           </div>
           <div className={projects.projectCard}>
             <Card title="SmartCharge Plus" description="Gain a better understanding of how much charging an EV can cost." github="https://github.com/barriosx/smartcharge-plus" demo="https://smartcharge-plus.herokuapp.com/" />
@@ -26,7 +24,6 @@ const Projects = () => (
         <div className={projects.project}>
           <div className={projects.projectImage}>
             <EzparknImage />
-            {/* <img src={ezparknGIF} className={projects.img} alt="EZParkn" /> */}
           </div>
           <div className={projects.projectCard}>
             <Card title="EZParkn" description="A parking reservation service for college students." github="https://github.com/CSC59939/EZPARKN" demo="https://ezparkn-ccny.herokuapp.com/" />
