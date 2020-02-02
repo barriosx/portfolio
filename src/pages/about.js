1640d310e855d5f430828e7b5639502b8938964e
barriosx <stevenbarrios03@gmail.com>
2020-02-02 15:38:08 -0500

    Reflect about page and resume pdf to new position gained

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,10 +19,10 @@ const About = () => (
         <p>I'm glad you want to get more familiar! Here's a quick overview of who I am:</p>
         <ul>
           <li>
-            I was born and currently live in NY, and am of Guatemalan and Salvadoran ethnicity <span role="img" aria-label="Guatemala">🇬🇹</span> <span role="img" aria-label="El Salvador">🇸🇻</span>
+            I was born and raised in New York, and I am of Guatemalan and Salvadoran ethnicity <span role="img" aria-label="Guatemala">🇬🇹</span> <span role="img" aria-label="El Salvador">🇸🇻</span>
           </li>
           <li>
-            I am currently undergoing the GOLD (Growth Opportunities for Leadership Development) program at <a className="link" href="https://www.coned.com/en">Con Edison</a>.
+            I am currently a <b>Front End Engineer</b> at <a className="link" href="https://www.coned.com/en">Con Edison</a>.
           </li>
           <li>  
             I like to travel <span role="img" aria-label="Plane Emoji">✈️</span> and I consider myself a foodie <span role="img" aria-label="Silverware Emoji">🍴</span>
